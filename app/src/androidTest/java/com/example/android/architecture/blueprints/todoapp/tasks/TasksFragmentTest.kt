9e3937c4d5f57e3e9dc2517243abecd17a0fe0d1
jonataslinoo <jonatas.linoo@gmail.com>
2024-09-27 12:51:38 -0300

    Adicionado novas dependencias para testes de integração usando mock, e adicionado alguns testes de navegação da tela principal.

---
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksFragmentTest.kt
@@ -0,0 +1,102 @@
+package com.example.android.architecture.blueprints.todoapp.tasks
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.ServiceLocator
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeAndroidTestRepository
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+
+@ExperimentalCoroutinesApi
+@MediumTest
+@RunWith(AndroidJUnit4::class)
+class TasksFragmentTest {
+
+    private lateinit var repository: TasksRepository
+
+    @Before
+    fun initRepository() {
+        repository = FakeAndroidTestRepository()
+        ServiceLocator.tasksRepository = repository
+    }
+
+    @After
+    fun clenUpDb() = runTest {
+        ServiceLocator.resetRepository()
+    }
+
+    @Test
+    fun clickTask_navigateToDetailFragmentOne() = runTest {
+        // Given - many tasks on the home screen
+        val task = Task("TITLE 1", "DESCRIPTION 1", false, "id1")
+        val task2 = Task("TITLE 2", "DESCRIPTION 2", true, "id2")
+
+        // Save the tasks on db
+        repository.saveTask(task)
+        repository.saveTask(task2)
+
+        // On the home screen
+        val scenario = launchFragmentInContainer<TasksFragment>(Bundle(), R.style.AppTheme)
+        val navController = mock(NavController::class.java)
+        scenario.onFragment {
+            Navigation.setViewNavController(it.view!!, navController)
+        }
+
+        // When - click on the first task
+        onView(withId(R.id.tasks_list))
+            .perform(
+                RecyclerViewActions.actionOnItem<RecyclerView.ViewHolder>(
+                    hasDescendant(withText("TITLE 1")), click()
+                )
+            )
+
+        // Then - verify that we navigate to the first detail screen
+        verify(navController).navigate(
+            TasksFragmentDirections.actionTasksFragmentToTaskDetailFragment("id1")
+        )
+    }
+
+    @Test
+    fun clickAddTaskButton_navigateToAddEditFragment() {
+        // Given - On the home screen
+        val scenario = launchFragmentInContainer<TasksFragment>(Bundle(), R.style.AppTheme)
+        val navController = mock(NavController::class.java)
+        scenario.onFragment {
+            Navigation.setViewNavController(it.view!!, navController)
+        }
+
+        // When - click on the add fab
+        onView(withId(R.id.add_task_fab))
+            .perform(click())
+
+        // Then -  Verify that we navigate to the add screen
+        verify(navController).navigate(
+            TasksFragmentDirections.actionTasksFragmentToAddEditTaskFragment(
+                null,
+                getApplicationContext<Context>().getString(R.string.add_task)
+            )
+        )
+    }
+}
